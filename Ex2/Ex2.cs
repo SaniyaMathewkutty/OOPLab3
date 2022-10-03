890707f2c6fe7c2c51fc7354d2d27431c8f998bf
Saniya Mathewkutty <S00223097@atu.ie>
2022-10-03 11:02:55 +0100

    Ex2

--- Ex2/Ex2.cs
+++ Ex2/Ex2.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+    Name: Saniya Mathewkutty  
+    Date: 03/10/2022
+    Description: Lab3 Ex2 - 
+
+   2.	Write a method that uses a switch statement to return the cost of a product,
+        where the method receives a product description as a string argument. (Costs given)
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
