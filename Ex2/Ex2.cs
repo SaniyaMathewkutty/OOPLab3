31d0019d0dc311bf9dec8df9b2b9b6e5d8ca338d
Saniya Mathewkutty <S00223097@atu.ie>
2022-10-03 11:14:15 +0100

    Ex2

--- Ex2/Ex2.cs
+++ Ex2/Ex2.cs
@@ -18,6 +18,43 @@ namespace Ex2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(ProductCost("Jeans"));
+            Console.WriteLine(ProductCost("Jacket"));
+            Console.WriteLine(ProductCost("Boots"));
+            Console.WriteLine(ProductCost("Scarves"));
+            Console.WriteLine(ProductCost("Belts"));
+            Console.WriteLine(ProductCost("Socks"));
+            Console.WriteLine(ProductCost("Shoes"));
+
+        }
+
+        static decimal ProductCost (string productDescription)
+        {
+            decimal price;
+
+            switch (productDescription)
+            {
+                case "Jeans":
+                    price = 67.99m; // "m" to convert decimal (float type) to literal
+                    break;
+                case "Jacket":
+                    price = 68.99m; 
+                    break;
+                case "Boots":
+                    price = 34.99m;
+                    break;
+                case "Scarves":
+                case "Belts":
+                case "Socks":
+                    price = 10m;
+                    break;
+
+                default:
+                    price = -999m;   // None of the above, signalling error.
+                    break;
+
+            }
+            return price;
         }
     }
 }
